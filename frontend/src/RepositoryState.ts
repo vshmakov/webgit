1d59683dd3ba178f616a8358a45182bcb545d244
VadimShmakov <shmakovvadim@mail.ru>
2021-11-20 10:16:09 +0500

    Added types

--- frontend/src/RepositoryState.ts
+++ frontend/src/RepositoryState.ts
@@ -14,7 +14,7 @@ export class RepositoryState {
     public status: StatusResult | null = null
     public branches: BranchesState | null = null
     public readonly stageAllFilesBeforeCommit: InMemoryFlag = new InMemoryFlag(true)
-    public commitMessageStorage = new LocalStorage<string>(LocalStorageKey.CommitMessage, '', this.path)
+    public commitMessageStorage: LocalStorage<string> = new LocalStorage<string>(LocalStorageKey.CommitMessage, '', this.path)
     public readonly precommitCommandStorage = new LocalStorage<string>(LocalStorageKey.PrecommitCommand, '', this.path)
     public readonly bitbucketRepositoryPathStorage = new LocalStorage<string>(LocalStorageKey.BitbucketRepositoryPath, '', this.path)
     public readonly jiraPathStorage = new LocalStorage<string>(LocalStorageKey.JiraPath, '', this.path)
@@ -68,7 +68,7 @@ export class RepositoryState {
 
     private setStatus(status: StatusResult): void {
         this.status = status
-        this.commitMessageStorage = new LocalStorage<string>(LocalStorageKey.CommitMessage, '', JSON.stringify([this.path, status.current]))
+        this.commitMessageStorage = new LocalStorage(LocalStorageKey.CommitMessage, '', JSON.stringify([this.path, status.current]))
     }
 
     private async loadBranches(): Promise<void> {
