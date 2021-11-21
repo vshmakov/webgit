84964bc1a254d0de2a96448f46ad708398a5150a
VadimShmakov <shmakovvadim@mail.ru>
2021-11-21 12:26:13 +0500

    Extracted Branch component

---
+++ frontend/src/Branch.tsx
@@ -0,0 +1,179 @@
+import {observer} from "mobx-react";
+import React, {ReactElement} from "react";
+import {LoadedRepositoryProps} from "./LoadedRepositoryProps";
+import {withSound} from "./WithSound";
+import {BranchSummaryBranch} from "simple-git";
+
+interface Props extends LoadedRepositoryProps {
+    branch: BranchSummaryBranch
+    index: number
+}
+
+export const Branch = observer(class extends React.Component<Props> {
+    public render(): ReactElement {
+        const {repository, branch, index} = this.props
+
+        return (
+            <tr>
+                <td>
+                    <input
+                        type='radio'
+                        name='current-branch'
+                        checked={this.isCurrentBranch()}
+                        onChange={() => withSound(repository.checkoutBranch(branch))}
+                        accessKey={(index + 1).toString()}
+                        disabled={repository.isDisabled.isChecked}/>
+                </td>
+                <td>
+                    {repository.getBranchName(branch)} {this.getTracking()}
+                </td>
+                <td>
+                    {this.getHideButton()}
+                    {this.getMergeIntoCurrentButton()}
+                    {this.getMergeTrackingButton()}
+                    {this.getPushButton()}
+                    {this.getCreateBitbucketPullRequestLink()}
+                </td>
+            </tr>
+        )
+    }
+
+    private getCreateBitbucketPullRequestLink(): ReactElement | null {
+        const {branch, status, repository} = this.props
+
+        if (branch.name !== status.current) {
+            return null
+        }
+
+        const path = repository.bitbucketRepositoryPathStorage.getValue()
+
+        if ('' === path) {
+            return null
+        }
+
+        return (
+            <a href={`${path}/pull-requests/new?source=${branch.name}&t=1`} role='button'>
+                Create bitbucket pull request
+            </a>
+        )
+    }
+
+    private getHideButton(): ReactElement | null {
+        const {branch, branches} = this.props
+
+        if (!branches.showHidden.isChecked) {
+            return null
+        }
+
+        return (
+            <button type='button' onClick={(): void => branches.toggleHide(branch.name)}>
+                {!branches.hiddenStorage.getValue().includes(branch.name) ? 'Hide' : 'Show'}
+            </button>
+        )
+    }
+
+    private getMergeIntoCurrentButton(): ReactElement | null {
+        if (this.isCurrentBranch()) {
+            return null
+        }
+
+        const {branch, repository, status} = this.props
+
+        return (
+            <button
+                type='button'
+                onClick={() => withSound(repository.mergeBranchIntoCurrent(branch))}
+                accessKey={this.isPreviousBranch() ? 'm' : undefined}>
+                Merge into {status.current}
+            </button>
+        )
+    }
+
+    private isPreviousBranch(): boolean {
+        const {branch, branches} = this.props
+        const history = branches.historyStorage.getValue();
+        const index = history.indexOf(branch.name)
+
+        if (-1 === index) {
+            return false
+        }
+
+        return index === history.length - 2
+    }
+
+    private getPushButton(): ReactElement | null {
+        if (!this.canPush()) {
+            return null
+        }
+
+        const {repository, status} = this.props
+
+        return (
+            <button type='button' onClick={() => withSound(repository.push())} accessKey='p'>
+                Push {null === status.tracking ? 'with upstream' : null}
+            </button>
+        )
+    }
+
+    private canPush(): boolean {
+        const {status} = this.props
+
+        const hasAheadCommits = null !== status.tracking
+            && 0 !== status.ahead
+
+        return this.isCurrentBranch()
+            && (null === status.tracking || hasAheadCommits)
+    }
+
+    private getMergeTrackingButton(): ReactElement | null {
+        if (!this.canMergeTrackingBranch()) {
+            return null
+        }
+
+        const {repository} = this.props
+
+        return (
+            <button type='button' onClick={() => withSound(repository.mergeTrackingBranch())} accessKey='l'>
+                Merge tracking
+            </button>
+        )
+    }
+
+    private canMergeTrackingBranch(): boolean {
+        const {status} = this.props
+
+        return this.isCurrentBranch()
+            && null !== status.tracking
+            && 0 !== status.behind
+    }
+
+    private isCurrentBranch(): boolean {
+        const {branch, status} = this.props
+
+        return branch.name === status.current
+    }
+
+    private getTracking(): string {
+        const {status} = this.props
+
+        if (!this.isCurrentBranch()) {
+            return ''
+        }
+
+        const parts = []
+
+        if (0 !== status.ahead) {
+            parts.push(`${status.ahead} ->`)
+        }
+
+        if (0 !== status.behind) {
+            parts.push(`<- ${status.behind}`)
+        }
+
+        if (null !== status.tracking) {
+            parts.push(`(${status.tracking})`)
+        }
+
+        return parts.join(' ')
+    }
+})
--- frontend/src/Branches.tsx
+++ frontend/src/Branches.tsx
@@ -6,211 +6,50 @@ import {preventDefault} from "./PreventDefault";
 import {withSound} from "./WithSound";
 import {BranchSummaryBranch} from "simple-git";
 import {Checkbox} from "./Checkbox";
+import {Branch} from "./Branch";
 
-export const Branches = observer(class extends React.Component<LoadedRepositoryProps> {
-    public render(): ReactElement {
-        const {repository, branches} = this.props
+export const Branches = observer(({repository, branches, status}: LoadedRepositoryProps): ReactElement => {
+    const rows = branches.sorted
+        .map((branch: BranchSummaryBranch, index: number): ReactElement => <Branch
+            branch={branch}
+            index={index}
+            repository={repository}
+            branches={branches}
+            status={status}
+            key={branch.name}/>)
 
-        return (
-            <div>
-                <h3>Branches</h3>
-                <form>
-                    <Checkbox label={`Show hidden (${branches.hidden.length})`} flag={branches.showHidden} key={JSON.stringify(branches.hidden)}/>
-                    <table>
-                        <thead>
-                        <tr>
-                            <th></th>
-                            <th>Branch</th>
-                            <th>Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {branches.sorted.map(this.renderBranch.bind(this))}
-                        </tbody>
-                    </table>
-                </form>
-                <Toggle label='Create' flag={repository.isBranchCreation}>
-                    <form onSubmit={preventDefault(() => withSound(repository.createBranch()))}>
-                        <input
-                            type="text"
-                            value={repository.newBranchName}
-                            onChange={(event) => repository.newBranchName = event.target.value}
-                            required={true}/>
-                        <button type="submit">
-                            Create
-                        </button>
-                    </form>
-                </Toggle>
-            </div>
-        )
-            ;
-    }
-
-    private getCreateBitbucketPullRequestLink(branch: BranchSummaryBranch): ReactElement | null {
-        if (branch.name !== this.props.status.current) {
-            return null
-        }
-
-        const path = this.props
-            .repository
-            .bitbucketRepositoryPathStorage
-            .getValue()
-
-        if (null === path) {
-            return null
-        }
-
-        return (
-            <a href={`${path}/pull-requests/new?source=${branch.name}&t=1`} role='button'>
-                Create bitbucket pull request
-            </a>
-        )
-    }
-
-    private renderBranch(branch: BranchSummaryBranch, index: number): ReactElement {
-        const {repository} = this.props
-
-        return (
-            <tr key={branch.name}>
-                <td>
+    return (
+        <div>
+            <h3>Branches</h3>
+            <form>
+                <Checkbox label={`Show hidden (${branches.hidden.length})`} flag={branches.showHidden}
+                          key={JSON.stringify(branches.hidden)}/>
+                <table>
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Branch</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {rows}
+                    </tbody>
+                </table>
+            </form>
+            <Toggle label='Create' flag={repository.isBranchCreation}>
+                <form onSubmit={preventDefault(() => withSound(repository.createBranch()))}>
                     <input
-                        type='radio'
-                        name='current-branch'
-                        checked={this.isCurrentBranch(branch)}
-                        onChange={() => withSound(this.props.repository.checkoutBranch(branch))}
-                        accessKey={(index + 1).toString()}
-                        disabled={repository.isDisabled.isChecked}/>
-                </td>
-                <td>
-                    {repository.getBranchName(branch)} {this.getTracking(branch)}
-                </td>
-                <td>
-                    {this.getHideButton(branch)}
-                    {this.getMergeIntoCurrentButton(branch)}
-                    {this.getMergeTrackingButton(branch)}
-                    {this.getPushButton(branch)}
-                    {this.getCreateBitbucketPullRequestLink(branch)}
-                </td>
-            </tr>
-        )
-    }
-
-    private getHideButton(branch: BranchSummaryBranch): ReactElement | null {
-        const {branches} = this.props
-
-        if (!branches.showHidden.isChecked) {
-            return null
-        }
-
-        return (
-            <button type='button' onClick={(): void => branches.toggleHide(branch.name)}>
-                {!branches.hiddenStorage.getValue().includes(branch.name) ? 'Hide' : 'Show'}
-            </button>
-        )
-    }
-
-    private getMergeIntoCurrentButton(branch: BranchSummaryBranch): ReactElement | null {
-        if (this.isCurrentBranch(branch)) {
-            return null
-        }
-
-        const {repository} = this.props
-
-        return (
-            <button
-                type='button'
-                onClick={() => withSound(repository.mergeBranchIntoCurrent(branch))}
-                accessKey={this.isPreviousBranch(branch) ? 'm' : undefined}>
-                Merge into {repository.status?.current}
-            </button>
-        )
-    }
-
-    private isPreviousBranch(branch: BranchSummaryBranch): boolean {
-        const history = this.props
-            .branches
-            .historyStorage
-            .getValue();
-        const index = history.indexOf(branch.name)
-
-        if (-1 === index) {
-            return false
-        }
-
-        return index === history.length - 2
-    }
-
-    private getPushButton(branch: BranchSummaryBranch): ReactElement | null {
-        if (!this.canPush(branch)) {
-            return null
-        }
-
-        const {repository, status} = this.props
-
-        return (
-            <button type='button' onClick={() => withSound(repository.push())} accessKey='p'>
-                Push {null === status.tracking ? 'with upstream' : null}
-            </button>
-        )
-    }
-
-    private canPush(branch: BranchSummaryBranch): boolean {
-        const {status} = this.props
-
-        const hasAheadCommits = null !== status.tracking
-            && 0 !== status.ahead
-
-        return this.isCurrentBranch(branch)
-            && (null === status.tracking || hasAheadCommits)
-    }
-
-    private getMergeTrackingButton(branch: BranchSummaryBranch): ReactElement | null {
-        if (!this.canMergeTrackingBranch(branch)) {
-            return null
-        }
-
-        const {repository} = this.props
-
-        return (
-            <button type='button' onClick={() => withSound(repository.mergeTrackingBranch())} accessKey='l'>
-                Merge tracking
-            </button>
-        )
-    }
-
-    private canMergeTrackingBranch(branch: BranchSummaryBranch): boolean {
-        const {status} = this.props
-
-        return this.isCurrentBranch(branch)
-            && null !== status.tracking
-            && 0 !== status.behind
-    }
-
-    private isCurrentBranch(branch: BranchSummaryBranch): boolean {
-        return branch.name === this.props.status.current
-    }
-
-    private getTracking(branch: BranchSummaryBranch): string {
-        const {status} = this.props
-
-        if (!this.isCurrentBranch(branch)) {
-            return ''
-        }
-
-        let tracking = ''
-
-        if (0 !== status.ahead) {
-            tracking += `${status.ahead} -> `
-        }
-
-        if (0 !== status.behind) {
-            tracking += ` <- ${status.behind}`
-        }
-
-        if (null !== status.tracking) {
-            tracking += ` (${status.tracking})`
-        }
-
-        return tracking
-    }
+                        type="text"
+                        value={repository.newBranchName}
+                        onChange={(event) => repository.newBranchName = event.target.value}
+                        required={true}/>
+                    <button type="submit">
+                        Create
+                    </button>
+                </form>
+            </Toggle>
+        </div>
+    )
+        ;
 })
