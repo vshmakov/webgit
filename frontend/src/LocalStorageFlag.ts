1f7172762bb74520eef75ba019463bd9d1fcb724
VadimShmakov <shmakovvadim@mail.ru>
2021-12-16 20:06:11 +0500

    [commit] Created flag by key

--- frontend/src/LocalStorageFlag.ts
+++ frontend/src/LocalStorageFlag.ts
@@ -1,8 +1,13 @@
 import {Flag} from "./Flag";
 import {LocalStorage} from "./LocalStorage";
+import {LocalStorageKey} from "./LocalStorageKey";
 
 export class LocalStorageFlag implements Flag {
     public constructor(private readonly localStorage: LocalStorage<boolean>) {
+    }
+
+    public static createByKey(key: LocalStorageKey, value: boolean, domain: string): LocalStorageFlag {
+        return new LocalStorageFlag(new LocalStorage(key, value, domain))
     }
 
     public get isChecked(): boolean {
