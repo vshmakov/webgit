6238a43eb61984ddd18c9f0716e0620a779fca6a
VadimShmakov <shmakovvadim@mail.ru>
2021-12-31 23:51:02 +0500

    [file] Refactored staging checking

--- frontend/src/Shared/IsFileStaged.ts
+++ frontend/src/Shared/IsFileStaged.ts
@@ -2,5 +2,5 @@ import {StatusResult, StatusResultRenamed} from "simple-git";
 
 export function isFileStaged(path: string, status: StatusResult): boolean {
     return status.staged.includes(path)
-        || status.renamed.map((renamed: StatusResultRenamed): string => renamed.to).includes(path);
+        || status.renamed.some((renamed: StatusResultRenamed): boolean => renamed.to === path)
 }
