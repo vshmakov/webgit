a26670db93ce8dcc86d0f825e34747324160800f
VadimShmakov <shmakovvadim@mail.ru>
2022-04-09 13:01:20 +0500

    [branch] Added special development access key

--- frontend/src/Branch/CheckoutRadio.tsx
+++ frontend/src/Branch/CheckoutRadio.tsx
@@ -5,21 +5,30 @@ import {RepositoryProps} from "../Repository/RepositoryProps";
 import React, {ReactElement} from "react";
 import {isCurrent} from "./IsCurrent";
 import {withSound} from "../Util/WithSound";
+import {BranchSummaryBranch} from "simple-git";
 
 export const CheckoutRadio = observer(({
                                            branch,
                                            index,
                                            repository
                                        }: BranchProps & IndexProps & RepositoryProps): ReactElement => {
-    const branchNumber = index + 1
-
     return (
         <input
             type="radio"
             name="current-branch"
             checked={isCurrent(branch, repository.status)}
             onChange={() => withSound(repository.checkout(branch.name))}
-            accessKey={branchNumber <= 9 ? branchNumber.toString() : undefined}
+            accessKey={getAccessKey(branch, index)}
             disabled={repository.isDisabled.isChecked}/>
     )
 })
+
+function getAccessKey(branch: BranchSummaryBranch, index: number): string | undefined {
+    if (['development'].includes(branch.name)) {
+        return 'd'
+    }
+
+    const branchNumber = index + 1
+
+    return branchNumber <= 9 ? branchNumber.toString() : undefined;
+}
