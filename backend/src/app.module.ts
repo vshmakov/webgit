b984025fe0bee8f77857bfb80ecfef466beb7b97
VadimShmakov <shmakovvadim@mail.ru>
2023-09-25 15:25:44 +0500

    Exposed static react

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ServeStaticModule} from "@nestjs/serve-static";
+import {join} from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '../../../../frontend/build'),
+        })
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
